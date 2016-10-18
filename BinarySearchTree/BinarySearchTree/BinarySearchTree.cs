e4b203e1d63886c0993a994eca244b2315290019
Sarah Busert <sarah.busert@hs-furtwangen.de>
2016-10-18 19:46:48 +0200

    Added sln

---
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearchTree
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+
+        public Node<T> LeftNode;
+        public Node<T> RightNode;
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+    }
+
+    public class BinarySearchTree<T> where T : IComparable
+    {
+        private Node<T> _globalRoot;
+        public Node<T> InsertNode(Node<T> root, T value)
+        {
+            if (root == null)
+            {
+                root = new Node<T>(value);
+                if(_globalRoot == null)
+                    _globalRoot = root;
+            }
+            else
+            {
+                if (value.CompareTo(root.Value) <= 0)
+                {
+                    root.LeftNode = InsertNode(root.LeftNode, value);
+                }
+                else if (value.CompareTo(root.Value) >= 0)
+                {
+                    root.RightNode = InsertNode(root.RightNode, value);
+                }
+            }
+            return root;
+        }
+
+        public IEnumerable<T> PreorderTraverseTree(Node<T> root)
+        {
+            if (root == null) yield break;
+            yield return root.Value;
+            foreach (var v in PreorderTraverseTree(root.LeftNode))
+            {
+                yield return v;
+            }
+
+            foreach (var v in PreorderTraverseTree(root.RightNode))
+            {
+                yield return v;
+            }
+        }
+
+        public void DeleteNode(ref Node<T> root, T value)
+        {
+            if (root == null) return;
+            if (root.Value.Equals(value))
+                root = Delete(ref root);
+            else if (value.CompareTo(root.Value) <= 0)
+                DeleteNode(ref root.LeftNode, value);
+            else if (value.CompareTo(root.Value) >= 0)
+            {
+                DeleteNode(ref root.RightNode, value);
+            }
+        }
+
+        private Node<T> Delete(ref Node<T> root)
+        {
+            if (_globalRoot == root)
+            {
+                Console.WriteLine("Deletion of root element is not allowed");
+                return root;
+            }
+            if (root.LeftNode == null && root.RightNode == null)
+            {
+                root = null;
+            }
+            else if (root.LeftNode == null)
+            {
+                root = root.RightNode;
+            }
+            else if (root.RightNode == null)
+            {
+               root = root.LeftNode;
+            }
+            else
+            {
+                Replace(ref root);
+            }
+            return root;
+        }
+
+        private void Replace(ref Node<T> root)
+        {
+            if (root.LeftNode == null)
+                root = root.RightNode;
+            else { Replace(ref root.LeftNode);}
+        }
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int min = 0;
+            const int max = 100;
+            var tree = new BinarySearchTree<int>();
+            var rnd = new int[10];
+
+            Node<int> root = null;
+
+            var rndNo = new Random();
+            for (var i = 0; i < rnd.Length; i++)
+            {
+                rnd[i] = rndNo.Next(min, max);
+            }
+
+            foreach (var r in rnd)
+            {
+                root = tree.InsertNode(root, r);
+            }
+
+            Console.WriteLine("input array: ");
+            Console.WriteLine(string.Join(",", rnd));
+            Console.ReadLine();
+            
+            var values = tree.PreorderTraverseTree(root).ToList();
+            Console.WriteLine("the trees preorder traversal: ");
+            Console.WriteLine(string.Join(",", values));
+            Console.ReadLine();
+
+            var rndItem = rnd[rndNo.Next(0, rnd.Length)];
+            Console.WriteLine(rndItem + " will be deleted from tree");
+            Console.ReadLine();
+            tree.DeleteNode(ref root, rndItem);
+           
+            var restTree = tree.PreorderTraverseTree(root).ToList();
+            Console.WriteLine("tree after deletion: ");
+            Console.WriteLine(string.Join(",", restTree));
+            Console.ReadLine();
+        }
+    }
+}
